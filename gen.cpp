4e471ddaa93b869409f691f0b75f4042b8120e07
zapwiz22 <kumar.jayant1916@gmail.com>
2026-02-01 22:37:04 +0530

    added gen.cpp && brute.cpp files

---
+++ gen.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long 
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int32_t main() {
+    int tests = 1;
+    cout << tests << endl;
+
+    // generation 
+    
+
+    return 0;
+}
+
+/* how to generate a tree
+  print uid(1,i-1) and i
+*/
+
+/* how to randomly shuffle a vector
+   // Initialize random number generator
+    random_device rd;
+    mt19937 g(rd());
+
+    // Shuffle the vector
+    shuffle(v.begin(), v.end(), g);
+*/
